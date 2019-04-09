e8b099bf3f59e946876e4d982778593d6066c06d
Fabian Wüthrich <me@fabwu.ch>
2019-04-09 08:21:34 +0200

    Sheet 5

--- sheet5/Tree.hs
+++ sheet5/Tree.hs
@@ -1,15 +1,15 @@
 module Tree where
-
+    
 data Tree t = Leaf | Node t (Tree t) (Tree t)
 
 bfs :: Tree t -> [t]
 bfs Leaf = []
 bfs (Node x l r) = aux [l,r] [x]
-    where
-        aux :: [Tree t] -> [t] -> [t]
-        aux [] a = a
-        aux (Leaf:q) a = aux q a
-        aux ((Node x l r):q) a = aux (q ++ [l,r]) (a ++ [x])
+  where
+    aux :: [Tree t] -> [t] -> [t]
+    aux [] a = a
+    aux (Leaf:q) a = aux q a
+    aux ((Node x l r):q) a = aux (q ++ [l,r]) (a ++ [x])
 
 mapTree :: (a -> b) -> Tree a -> Tree b
 mapTree f Leaf = Leaf
